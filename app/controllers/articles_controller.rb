aa463144dd8906f96fe53956e52b2c055ac93e87
Jared <jared.alonzo@icloud.com>
2018-08-04 00:51:00 -0700

    Added links to view all articles and a button link to create a new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  # useful: convention for order of methods in all controller classes - index, show, new, edit, create, update, destroy
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
