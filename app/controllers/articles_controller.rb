ac18de783d112bd61eaa07918eeab8b113fca427
Jared <jared.alonzo@icloud.com>
2018-08-05 21:34:34 -0700

    Added editing and deleting articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,13 +9,40 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    # useful: Article.new returns true or false... based on the validates section in corresponding models
+    if @article.save
+      redirect_to @article
+    else
+      # useful: use render instead of redirect_to in order to avoid issuing another request for a new article from the server
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  # useful: uses ActiveRecord:Base methods which supplies rails models functions (C-reate,R-ead,U-pdate, D-estroy)
+  validates :title, presence: true,
+                    length: { minimum: 5}
 end
